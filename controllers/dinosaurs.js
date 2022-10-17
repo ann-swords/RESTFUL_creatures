6ab8c3bd26e9570314c401f8b1852367dfe34ac0
Anwaar <anwaaralsyouf@gmail.com>
2022-10-17 11:50:10 +0300

    DELETE and PUT added to dinosaurs

--- controllers/dinosaurs.js
+++ controllers/dinosaurs.js
@@ -39,4 +39,43 @@ router.post('/',(req, res)=>{
     res.redirect('/dinosaurs')
 })
 
+router.delete('/:idx', (req, res)=>{
+    console.log('This is my Req Param Object', req.params)
+    let dinosaurs = fs.readFileSync('./dinosaurs.json')
+    let dinoData =JSON.parse(dinosaurs)
+
+    dinoData.splice(req.params.idx, 1)
+
+    fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinoData))
+
+    res.redirect('/dinosaurs')
+})
+
+
+router.get('/edit/:idx', (req, res) =>{
+    //Grab dino data
+    let dinosaurs = fs.readFileSync('./dinosaurs.json')
+    let dinoData = JSON.parse(dinosaurs)
+
+    //Display edit page
+    res.render('dinosaurs/edit', {dino: dinoData[req.params.idx], dinoId: req.params.idx})
+
+})
+
+router.put('/:dinoId', (req, res)=>{
+    //Grab all Dino data
+    let dinosaurs = fs.readFileSync('./dinosaurs.json')
+    //Parse JSON data into JS Object VVVV
+    let dinoData = JSON.parse(dinosaurs)
+
+    //update our dinosaurs with form data
+    dinoData[req.params.dinoId].name = req.body.name
+    dinoData[req.params.dinoId].type = req.body.type
+
+    //update our json filr with new data
+    fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinoData))
+
+    //redirect to home page
+    res.redirect('/dinosaurs')
+})
 module.exports = router
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const ejsLayouts = require('express-ejs-layouts')
 const app = express()
+const methodOverride = require('method-override')
 
 app.set('view engine', 'ejs')
+
 //Middleware
 app.use(ejsLayouts)
 app.use(express.urlencoded({extended:false})) //for the request body in the form
+app.use(methodOverride('_method'))
 
 //Controllers
 app.use('/dinosaurs', require('./controllers/dinosaurs'))
